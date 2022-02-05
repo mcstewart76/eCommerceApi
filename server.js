cd52b2f5349944950518f1cc50b09f3c322f0294
mcstewart76 <mcstewart76@gmail.com>
2022-02-05 18:47:49 -0500

    Added model relations, added model info, added sequelize and seeding process

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('./config/connection');
+const { Category, Product, Tag } = require('./models');
+
+//For seeding
+const seedTags = require('./seeds/tag-seeds')
+const seedProductTags = require('./seeds/product-tag-seeds')
+const seedProducts = require('./seeds/product-seeds')
+const seedCategories = require('./seeds/category-seeds')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +18,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: true }).then(() => {
+
+  seedCategories();
+  seedProducts();
+  seedProductTags();
+  seedTags();
+})
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
